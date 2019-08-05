b7f9aee6f13db72cecb66e0a7417f1380761b20a
Mohini Modi <Mohini.Modi@resmed.com>
2019-08-05 12:35:30 -0700

    stetting up cntrollers fetchign the article, checking no duplicates save function

---
+++ controllers/fetch.js
@@ -0,0 +1,31 @@
+var db = require("../models");
+var scrape = require("../scripts/scrape");
+
+module.exports = {
+  scrapeHeadlines: function(req, res) {
+    return scrape()
+      .then(function(articles) {
+        // inserting articles into the db
+        return db.Headline.create(articles);
+      })
+      .then(function(dbHeadline) {
+        if (dbHeadline.length === 0) {
+          res.json({
+            message: "No new articles today. Check back tomorrow!"
+          });
+        }
+        else {
+          // Otherwise send back a count of how many new articles we got
+          res.json({
+            message: "Added " + dbHeadline.length + " new articles!"
+          });
+        }
+      })
+      .catch(function(err) {
+        // This query won't insert articles with duplicate headlines, but it will error after inserting the others
+        res.json({
+          message: "Scrape complete!!"
+        });
+      });
+  }
+};
