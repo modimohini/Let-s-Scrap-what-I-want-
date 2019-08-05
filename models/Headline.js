0c98137904c3e183f23745478b088d190128248e
Mohini Modi <Mohini.Modi@resmed.com>
2019-08-05 13:00:18 -0700

    models schema of the mogoDB setup for headline

---
+++ models/Headline.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var headlineSchema = new Schema({
+  headline: {
+    type: String,
+    required: true,
+    unique: { index: { unique: true } }
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  // summary, url, headline a string, must be entered
+  url: {
+    type: String,
+    required: true
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  }
+});
+
+var Headline = mongoose.model("Headline", headlineSchema);
+
+module.exports = Headline;
